6656aacb56e18856e6e18c392a56f572307b2abc
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2026-03-06 21:17:16 +0000

    chore: Add login button

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,9 +1,14 @@
+import { SignOutIcon, UserIcon } from '@phosphor-icons/react';
+import { useAuthSuspense } from '@repo/auth/tanstack/hooks';
+import { Button } from '@repo/ui/components/button';
 import { Clock } from '@repo/ui/components/clock';
+import { Link } from '@tanstack/react-router';
 
 import { KeyboardToggle } from './keyboard-toggle';
 import { ThemeToggle } from './theme-toggle';
 
 export function Header() {
+    const { user } = useAuthSuspense();
     return (
         <header className="flex items-center justify-end gap-2 p-4">
             <div className="grow">
@@ -11,6 +16,15 @@ export function Header() {
             </div>
             <KeyboardToggle />
             <ThemeToggle />
+            <Link to="/login">
+                <Button variant="outline" size="icon">
+                    {user ? (
+                        <SignOutIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
+                    ) : (
+                        <UserIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
+                    )}
+                </Button>
+            </Link>
         </header>
     );
 }
