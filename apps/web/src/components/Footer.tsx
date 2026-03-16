447d6dc122361a4cf14d13f93031627548fb1cfd
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2026-03-16 15:32:23 +0000

    chore: Move Clock + Add current logged-in user

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,7 +1,10 @@
+import { Clock } from '@repo/ui/components/clock';
+
 export function Footer() {
     return (
-        <footer className="absolute bottom-0 left-0 flex items-center justify-start gap-2 p-4 text-sm text-accent">
-            <span>© 2026 Data Science Imperial. All rights reserved.</span>
+        <footer className="absolute bottom-0 left-0 flex w-full items-center justify-between gap-2 p-4 text-sm text-accent">
+            <span className="grow">© 2026 Data Science Imperial. All rights reserved.</span>
+            <Clock />
         </footer>
     );
 }
--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { CircleNotchIcon, SignOutIcon, UserIcon } from '@phosphor-icons/react';
 import { useAuthSuspense } from '@repo/auth/tanstack/hooks';
 import { Button } from '@repo/ui/components/button';
-import { Clock } from '@repo/ui/components/clock';
 import { Link } from '@tanstack/react-router';
 import { Suspense } from 'react';
 
@@ -14,7 +13,6 @@ export function Header() {
         <header className="absolute top-0 left-0 flex min-w-screen items-center justify-end gap-2 p-4">
             <Link to="/" className="flex grow flex-row gap-3 font-mono">
                 Gemma Shop
-                <Clock />
             </Link>
             <KeyboardToggle />
             <ThemeToggle />
@@ -28,7 +26,7 @@ export function Header() {
                         {user ? (
                             <>
                                 <SignOutIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
-                                {user.email}
+                                <span className="hidden lg:inline">{user.email}</span>
                             </>
                         ) : (
                             <UserIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
