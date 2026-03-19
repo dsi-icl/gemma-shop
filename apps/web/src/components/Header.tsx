e313b0c8037b0276cd6030af77b28a809a7ff5b6
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2026-03-19 20:12:27 +0000

    chore: Tweak Header for signout

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,8 @@
 import { CircleNotchIcon, SignOutIcon, UserIcon } from '@phosphor-icons/react';
+import authClient from '@repo/auth/auth-client';
 import { useAuthSuspense } from '@repo/auth/tanstack/hooks';
 import { Button } from '@repo/ui/components/button';
-import { Link } from '@tanstack/react-router';
+import { Link, useNavigate } from '@tanstack/react-router';
 import { Suspense } from 'react';
 
 import { KeyboardToggle } from './KeyboardToggle';
@@ -9,6 +10,7 @@ import { ThemeToggle } from './ThemeToggle';
 
 export function Header() {
     const { user } = useAuthSuspense();
+    const naviate = useNavigate();
     return (
         <header className="absolute top-0 left-0 flex min-w-screen items-center justify-end gap-2 p-4">
             <Link to="/" className="flex grow flex-row gap-3 font-mono">
@@ -16,24 +18,27 @@ export function Header() {
             </Link>
             <KeyboardToggle />
             <ThemeToggle />
-            <Link to="/login">
-                <Button variant="outline">
-                    <Suspense
-                        fallback={
-                            <CircleNotchIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 animate-spin transition-all" />
-                        }
+            <Suspense
+                fallback={
+                    <CircleNotchIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 animate-spin transition-all" />
+                }
+            >
+                {user ? (
+                    <Button
+                        variant="outline"
+                        onClick={() => authClient.signOut().then(() => naviate({ to: '/' }))}
                     >
-                        {user ? (
-                            <>
-                                <SignOutIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
-                                <span className="hidden lg:inline">{user.email}</span>
-                            </>
-                        ) : (
+                        <SignOutIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
+                        <span className="hidden lg:inline">{user.email}</span>
+                    </Button>
+                ) : (
+                    <Link to="/login">
+                        <Button variant="outline">
                             <UserIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
-                        )}
-                    </Suspense>
-                </Button>
-            </Link>
+                        </Button>
+                    </Link>
+                )}
+            </Suspense>
         </header>
     );
 }
