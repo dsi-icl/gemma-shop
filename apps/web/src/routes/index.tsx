c130537a012561a87c37ada476493e0ec0717cc4
Florian Guitton <2495155+fguitton@users.noreply.github.com>
2026-03-26 12:56:07 +0000

    chore: Redirect non-logged-in visits for walls to gallery

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,20 @@
+import { authQueryOptions } from '@repo/auth/tanstack/queries';
 import { Button } from '@repo/ui/components/button';
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute, Link, redirect } from '@tanstack/react-router';
 
 import { publishedProjectsQueryOptions } from '~/server/projects.queries';
 
 export const Route = createFileRoute('/')({
     component: HomePage,
+    beforeLoad: async ({ context, search }) => {
+        const { w } = search as { w?: string };
+        if (w) {
+            const user = await context.queryClient.ensureQueryData(authQueryOptions());
+            if (!user) {
+                throw redirect({ to: '/gallery', search: { w } });
+            }
+        }
+    },
     loader: ({ context }) => {
         context.queryClient.ensureQueryData(publishedProjectsQueryOptions());
     }
